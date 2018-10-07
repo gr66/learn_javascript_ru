58d5dd6153d1c5136584e33d74c4aeb3170e75df
Gregory Kravchenko <2crazymail@gmail.com>
2018-10-07 22:21:29 +0200

    Render articles array

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,13 +1,12 @@
 import React from "react"
 import Article from "./article"
 
-export default function ArticleList(props) {
-    var articles = props.articles
+export default function ArticleList({articles}) {
+    const articleElements = articles.map(article => <li key={article.id}><Article article={article}/></li>)
+
     return (
         <ul>
-            <li><Article article={articles[0]}/></li>
-            <li><Article article={articles[1]}/></li>
-            <li><Article article={articles[2]}/></li>
+            {articleElements}
         </ul>
     )
 }
