fc9e15d0ca91a9aa75a3de452dbbccd8283e029c
Gregory Kravchenko <2crazymail@gmail.com>
2018-10-09 00:14:55 +0200

    Small refactoring

--- src/CommentsList.js
+++ src/CommentsList.js
@@ -1,10 +1,41 @@
-import React from "react"
+import React, {Component} from "react"
 import Comment from "./Comment"
 
-export default function CommentsList({comments}) {
-    const commentsElement = comments.map(comment => <div key={comment.id}><Comment comment={comment}/></div>)
+export default class CommentsList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen: false
+        }
+    }
 
-    return (
-        <ul>{commentsElement}</ul>
-    )
+    render() {
+        const {isOpen} = this.state
+        return (
+            <commentsSection>
+                <button onClick={this.toggleOpen}>
+                    {isOpen ? "Hide" : "Show"} comments
+                </button>
+                {this.getComments()}
+            </commentsSection>
+        )
+    }
+
+    toggleOpen = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    getComments() {
+        if (!this.state.isOpen) return null
+        const {comments} = this.props
+        if (!comments || !comments.length) return "No comments yet"
+        return (
+            <ul>
+                {comments.map(comment => <li key={comment.id}><Comment comment={comment} /></li>)}
+            </ul>
+        )
+    }
+
 }
--- src/article.js
+++ src/article.js
@@ -10,6 +10,7 @@ export default class Article extends Component {
             isOpenComments: false
         }
     }
+
     render() {
         const {article} = this.props
         const {isOpen} = this.state
@@ -17,7 +18,7 @@ export default class Article extends Component {
         return (
             <div>
                 <h3>{article.title}</h3>
-                <button onClick = {this.toggleOpen}>
+                <button onClick={this.toggleOpen}>
                     {isOpen ? '^' : 'Open'}
                 </button>
                 <div>{this.getBody()}</div>
@@ -27,17 +28,13 @@ export default class Article extends Component {
 
     getBody() {
         if (!this.state.isOpen) return null
-        const {isOpenComments} = this.state
 
         const {article} = this.props
         return (
             <articleSection>
                 {article.text}
                 <div>
-                    <button onClick={this.toggleComments}>
-                        {isOpenComments ? "Hide" : "Show"} comments
-                    </button>
-                    {this.getComments(article)}
+                    <CommentsList comments={article.comments}/>
                 </div>
             </articleSection>
         )
@@ -47,17 +44,5 @@ export default class Article extends Component {
         this.setState({
             isOpen: !this.state.isOpen
         })
-    }
-
-    toggleComments = () => {
-        this.setState({
-            isOpenComments: !this.state.isOpenComments
-        })
-    }
-
-    getComments(article) {
-        if (!this.state.isOpenComments || article.comments === undefined) return null
-
-        return <CommentsList comments={article.comments}/>
     }
 }
