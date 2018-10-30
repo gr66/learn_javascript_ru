64990b1729a8d3cb68e7bf3cbd28923dd6faa5cb
Gregory Kravchenko <2crazymail@gmail.com>
2018-10-30 15:29:27 +0100

    For external library

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
 import ArticleList from "./ArticleList"
+import ArticleChart from "./ArticleChart"
 
 class App extends Component {
     render() {
         return (
             <div>
                 <ArticleList articles={this.props.articles} />
+                <ArticleChart articles={this.props.articles} />
             </div>
         );
     }
---
+++ src/components/ArticleChart.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class ArticleChart extends Component {
+    componentDidMount() {
+        //work with this.refs.chart
+    }
+
+    componentWillReceivProps(nextProps) {
+        //update chart for new article
+    }
+
+    render() {
+        return (
+            <div ref="chart"/>
+        );
+    }
+
+    componentWillUnmount() {
+        //claenup
+    }
+}
+
+export default ArticleChart;
