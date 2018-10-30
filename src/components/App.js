dba371a22df56eab93e77fbcac104a8b3aff3fe9
Gregory Kravchenko <2crazymail@gmail.com>
2018-10-30 16:22:09 +0100

    Add user form

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
 import ArticleList from "./ArticleList"
 import ArticleChart from "./ArticleChart"
+import UserForm from "./UserForm"
 
 class App extends Component {
     render() {
         return (
             <div >
+                <UserForm />
                 <ArticleList articles = {this.props.articles} />
                 <ArticleChart articles = {this.props.articles} />
             </div >
